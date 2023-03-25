ee2c1d04b193c74f9d9555aca5ddb399599ff873
DiazFernandito <diaz.fernandito@gmail.com>
2023-03-24 23:50:55 -0300

    Ejercicio 14 - JS + HTML

---
+++ recuperarParrafo2.js
@@ -0,0 +1,6 @@
+// Función llamada desde el body del documento HTML para recuperar el contendo del parrafo 2
+function recuperar() {
+    document.getElementById("parrafo2").innerHTML = "Al Hacer click sobre el botón de abajo cambia el contenido de este mensaje. Inténtelo !!!" ;
+    return ;
+}
+
